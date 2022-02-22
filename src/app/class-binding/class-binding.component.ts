719ce8f695887add2e7d86e4606d8ab8d19b6a06
Zartab Nakhwa <zartab@codewithz.com>
2022-02-22 18:29:49 +0530

    Class Binding

--- src/app/class-binding/class-binding.component.ts
+++ src/app/class-binding/class-binding.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassBindingComponent implements OnInit {
 
+
+  isActive = true;
+
+  bgColor = "gold"
+
   constructor() { }
 
   ngOnInit(): void {
