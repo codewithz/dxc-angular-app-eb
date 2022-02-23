dc1a45370c52027e5a45aedd6ca3d15d77384677
Zartab Nakhwa <zartab@codewithz.com>
2022-02-23 16:44:48 +0530

    Custome Pipes -1

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PipesComponent implements OnInit {
 
+  longDescription: string = `
+  Angular is an application design framework and development platform for creating efficient and sophisticated single-page apps.
+
+  These Angular docs help you learn and use the Angular framework and development platform, from your first application to optimizing complex single-page apps for enterprises. Tutorials and guides include downloadable examples to accelerate your projects.
+  `
   course = {
     title: 'Angular 12 In Depth Training',
+    description: this.longDescription,
     rating: 4.567,
     students: 10023,
     price: 200.34,
