dcb1574d658eb6d951607336d8f0aa671e0c41fc
Zartab Nakhwa <zartab@codewithz.com>
2022-02-25 15:20:38 +0530

    ngFor- Track By

--- src/app/directive-for-track/directive-for-track.component.ts
+++ src/app/directive-for-track/directive-for-track.component.ts
@@ -1,4 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
+
+interface Course {
+  id: number,
+  name: string
+}
 
 @Component({
   selector: 'app-directive-for-track',
@@ -6,20 +12,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directive-for-track.component.css']
 })
 export class DirectiveForTrackComponent {
-  courses = [
-    { id: 1, name: 'Course 1' },
-    { id: 2, name: 'Course 2' },
-    { id: 3, name: 'Course 3' },
-    { id: 4, name: 'Course 4' },
-  ]
+  courses: any;
+
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'Course 1' },
+      { id: 2, name: 'Course 2' },
+      { id: 3, name: 'Course 3' },
+      { id: 4, name: 'Course 4' },
+    ]
+  }
 
   onAdd() {
     this.courses.push({ id: 5, name: 'Course 5' });
   }
 
-  onRemove(course: { id: number, name: string }) {
+  onRemove(course: Course) {
     let index = this.courses.indexOf(course);
     this.courses.splice(index, 1);
+  }
+
+  onUpdate(course: Course) {
+    let index = this.courses.indexOf(course);
+    let courseClone = this.courses[index];
+    courseClone.name = 'Test Course';
+    this.courses[index] = courseClone
+
+  }
+
+  trackCourse(index: number, course: Course) {
+    return course ? course.id : undefined;
   }
 
 }
