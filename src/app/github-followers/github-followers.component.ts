3df98c1843312a3dc80639f995962b49d4b1ced1
Zartab Nakhwa <zartab@codewithz.com>
2022-03-03 15:03:46 +0530

    Routing: Query Parameters

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from './github.service';
 import { GithubUser } from './github-user.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-github-followers',
@@ -10,9 +11,18 @@ import { GithubUser } from './github-user.model';
 export class GithubFollowersComponent implements OnInit {
 
   followers: GithubUser[] = [];
-  constructor(private service: GithubService) { }
+  constructor(private route: ActivatedRoute, private service: GithubService) { }
 
   ngOnInit(): void {
+
+    this.route.queryParamMap
+      .subscribe(
+        (params) => {
+          console.log("Page :", params.get('page'))
+          console.log("Order :", params.get('order'))
+        }
+      )
+
     this.service.getFollowers()
       .subscribe(
         (response) => {
